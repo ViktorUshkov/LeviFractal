1c3f70a0518ae8f6b6969f3682f06e82f1de23d1
ViktorUshkov <63870567+ViktorUshkov@users.noreply.github.com>
2020-04-20 09:43:50 +0300

    Update graphics.c

--- graphics.c
+++ graphics.c
@@ -23,3 +23,10 @@ void Draw(HDC hdc, int itr, line_t base) {
 	}
 	DeleteObject(hPen);
 }
+
+point_t get_point(line_t line) {
+	point_t new_point;
+	new_point.x = (line.beg.x + line.end.x) / 2 - (line.end.y - line.beg.y) / 2;
+	new_point.y = (line.beg.y + line.end.y) / 2 + (line.end.x - line.beg.x) / 2;
+	return new_point;
+}
