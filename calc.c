e9bb19b6fa67a8796180258c3b248d8089fe4e44
ViktorUshkov <63870567+ViktorUshkov@users.noreply.github.com>
2020-04-20 09:44:38 +0300

    Delete calc.c

--- calc.c
+++
@@ -1,8 +0,0 @@
-#include "calc.h"
-
-point_t get_point(line_t line) {
-	point_t new_point;
-	new_point.x = (line.beg.x + line.end.x) / 2 - (line.end.y - line.beg.y) / 2;
-	new_point.y = (line.beg.y + line.end.y) / 2 + (line.end.x - line.beg.x) / 2;
-	return new_point;
-}
